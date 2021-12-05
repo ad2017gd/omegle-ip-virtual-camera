8fa054ab64f888bb8edf5ef9f4f4cec352a24aba
ad <39013925+ad2017gd@users.noreply.github.com>
2021-12-05 15:36:35 +0200

    the

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const express = require('express')
 const app = express()
 const fetch = require('fetch')
 const fs = require('fs');
+const CountryLanguage = require('country-language')
 
 app.get('/:ip', (req, res) => {
     fetch.fetchUrl(`http://ip-api.com/json/${req.params.ip}`, function (error, meta, body) {
         let data = JSON.parse(body.toString());
         delete data.status;
+        let cncode = "en";
+        CountryLanguage.getCountry(data.countryCode, function (err, country) {
+            if (err) {
+            } else {
+            console.log(country);
+            if(country.languages[0]) cncode = country.languages[0]["iso639_1"];
+            if(!country.languages[0]) cncode = country.languages["iso639_1"];
+            }
+          });
         delete data.countryCode;
         data.region = data.regionName;
         delete data.timezone;
@@ -24,7 +34,18 @@ app.get('/:ip', (req, res) => {
         let year = Math.floor(2022+Math.random()*4);
         fs.writeFileSync("fatality_chinese.txt", `您將於 ${year} 年 ${month+1} 月 ${day} 日被處決 `)
         fs.writeFileSync("fatality_english.txt", `You will be executed on ${day} ${["January","February","March","April","May","June","July","August","September","October", "November", "December"][month]}, ${year} `)
-        res.send("OK");
+        fetch.fetchUrl(`https://api.allorigins.win/get?url=${encodeURIComponent("https://translate-service.scratch.mit.edu/translate?language="+cncode+"&text="+encodeURIComponent("Assassination Date"))}`, function (error2,meta2,body2) {
+            if(error2 || body2.includes("Bad Request")) {
+                fs.writeFileSync("translated.txt", "Assassination Date");
+            } else {
+                try {
+                    fs.writeFileSync("translated.txt",JSON.parse(JSON.parse(body2).contents).result);
+                } catch (e) {
+                    fs.writeFileSync("translated.txt", "Assassination Date");
+                }
+                }
+            res.send("OK");
+        })
     });
 })
 
