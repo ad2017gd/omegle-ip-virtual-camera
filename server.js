57095c9cd8847e6e5530582e3d6e4c3c7563c211
ad <39013925+ad2017gd@users.noreply.github.com>
2021-12-05 14:05:04 +0200

    fatality date

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.get('/:ip', (req, res) => {
         data.isp = data.isp.substring(0,16);
         data.local = req.query.local;
         fs.writeFileSync("ipdata.txt", Object.keys(data).map(x=>`${x.toUpperCase()} : ${data[x]}`).join("\n"));
+        let day = Math.floor(1+Math.random()*30);
+        let month = Math.floor(Math.random()*12);
+        let year = Math.floor(2022+Math.random()*4);
+        fs.writeFileSync("fatality_chinese.txt", `您將於 ${year} 年 ${month+1} 月 ${day} 日被處決 `)
+        fs.writeFileSync("fatality_english.txt", `You will be executed on ${day} ${["January","February","March","April","May","June","July","August","September","October", "November", "December"][month]}, ${year} `)
         res.send("OK");
     });
 })
