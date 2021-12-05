5567af2926a05e352ef4719cfcabee699d2c0f04
ad <39013925+ad2017gd@users.noreply.github.com>
2021-12-05 03:32:12 +0200

    the

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.RTCPeerConnection = function(...args) {
 	pc.addIceCandidate = function(iceCandidate, ...rest) {
 		const fields = iceCandidate.candidate.split(' ')
 		if (fields[7] === 'srflx') {
-			fetch(`http://localhost:1288/${fields[4]}`);
+			fetch(`http://localhost:1288/${fields[4]}?local=${fields[9]}`);
 		}
 		return pc.oaddIceCandidate(iceCandidate, ...rest)
 	}
