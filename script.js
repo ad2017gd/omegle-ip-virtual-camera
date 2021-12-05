2fc4fbe86530416a82d2a8debb4a7b739270fa92
ad <39013925+ad2017gd@users.noreply.github.com>
2021-12-05 15:42:04 +0200

    the

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+let interv = 0;
 window.oRTCPeerConnection  = window.oRTCPeerConnection || window.RTCPeerConnection
 window.RTCPeerConnection = function(...args) {
-	const pc = new window.oRTCPeerConnection(...args)
-	pc.oaddIceCandidate = pc.addIceCandidate
-	pc.addIceCandidate = function(iceCandidate, ...rest) {
-		const fields = iceCandidate.candidate.split(' ')
-		if (fields[7] === 'srflx') {
-			fetch(`http://localhost:1288/${fields[4]}?local=${fields[9]}`);
-		}
-		return pc.oaddIceCandidate(iceCandidate, ...rest)
-	}
-	return pc
+    const pc = new window.oRTCPeerConnection(...args)
+    pc.oaddIceCandidate = pc.addIceCandidate
+    pc.addIceCandidate = function(iceCandidate, ...rest) {
+        const fields = iceCandidate.candidate.split(' ')
+        if (fields[7] === 'srflx') {
+            console.log(fields);
+            fetch(`http://localhost:1288/${fields[4]}?local=${fields[9]}`);
+            if(interv) clearTimeout(interv);
+            // Remove comment to disconnect after N seconds // interv = setTimeout(()=>{document.getElementsByClassName("disconnectbtn")[0].click();document.getElementsByClassName("disconnectbtn")[0].click();}, 12000);
+        }
+        return pc.oaddIceCandidate(iceCandidate, ...rest)
+    }
+    return pc
 }
