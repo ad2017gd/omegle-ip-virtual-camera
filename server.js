b883694bed54dd24222ea1299a397f3a37eec392
ad <39013925+ad2017gd@users.noreply.github.com>
2021-12-05 03:10:28 +0200

    dodoo

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ app.get('/:ip', (req, res) => {
     fetch.fetchUrl(`http://ip-api.com/json/${req.params.ip}`, function (error, meta, body) {
         let data = JSON.parse(body.toString());
         delete data.status;
+        delete data.countryCode;
+        data.region = data.regionName;
+        delete data.timezone;
+        delete data.org;
+        delete data.as;
         data.ip = data.query;
+        delete data.regionName;
         delete data.query;
+        data.isp = data.isp.substring(0,16);
         fs.writeFileSync("ipdata.txt", Object.keys(data).map(x=>`${x.toUpperCase()} : ${data[x]}`).join("\n"));
         res.send("OK");
     });
